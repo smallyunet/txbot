808061967607d568f00fd34edf9e4525c61f131f
smallyunet <dev.wangyu@gmail.com>
2022-11-23 10:43:12 +0800

    add start cfg info

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import os
 import datetime
 
 import telegram as tg
+import config as cfg
 import task as t
 
 if __name__ == '__main__':
     try:
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        msg = f'Mail list type: {cfg.mail_list_type}, Telegram bot enabled: {cfg.telegram_enable}, Binance enabled: {cfg.binance_enable}'
+        print(f'Start at {now}, {msg}')
+        tg.send_by_bot(msg)
+
         t.job()
+
     except KeyboardInterrupt:
-        print("I'm exiting...", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        tg.send_by_bot("I'm exiting...")    
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("I'm exiting...", now)
+        tg.send_by_bot("I'm exiting...")
+
         try:
             sys.exit(0)
         except SystemExit:
--- task.py
+++ task.py
@@ -7,8 +7,6 @@ import bi123 as bm
 
 
 def job():
-    print("I'm working...", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    tg.send_by_bot("I'm working...")
 
     schedule.every().hour.at(":03").do(bm.get_mail)
 
