3a4231cc44e41f728ae40995139f767ad6aab494
smallyunet <dev.wangyu@gmail.com>
2022-11-26 21:07:30 +0800

    add a example config file

---
+++ config-example.py
@@ -0,0 +1,17 @@
+mail_address = ''
+mail_password = ''
+mail_server = ''
+mail_server_port = 0
+
+# 'ALL' or '(UNSEEN)'
+mail_list_type = '(UNSEEN)'
+
+job_minute = False
+
+telegram_enable = True
+telegram_api_token = ''
+telegram_chat_id = ''
+
+binance_enable = True
+biance_api_key = ''
+biance_secrect_key = ''
