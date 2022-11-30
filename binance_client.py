df8f55c35331835ec480499973d1203884d0c7e1
smallyunet <dev.wangyu@gmail.com>
2022-11-30 21:15:57 +0800

    catch error in binance client

--- binance_client.py
+++ binance_client.py
@@ -7,6 +7,19 @@ import config as cfg
 import telegram as tg
 
 
+def getBalance(spot, symbol, msgPrefix):
+    balances = spot.account()['balances']
+    symbol_balance = float([x['free']
+                            for x in balances if x['asset'] == symbol][0])
+    usdt_balance = float([x['free']
+                          for x in balances if x['asset'] == 'USDT'][0])
+    msg = f'{msgPrefix}\n'
+    msg += f'{symbol} balance: {symbol_balance}\n'
+    msg += f'USDT balance: {usdt_balance}\n'
+    tg.send_by_bot(msg)
+    return symbol_balance, usdt_balance
+
+
 def make_order(type, symbol, quoteOrderQty=0):
     if not cfg.binance_enable:
         return
@@ -17,50 +30,41 @@ def make_order(type, symbol, quoteOrderQty=0):
     msg += f'quoteOrderQty: {quoteOrderQty}\n'
     tg.send_by_bot(msg)
 
-    spot = Spot()
-    spot = Spot(key=cfg.biance_api_key, secret=cfg.biance_secrect_key)
-    client = Client(cfg.biance_api_key, cfg.biance_secrect_key)
-
-    balances = spot.account()['balances']
-    symbol_balance = float([x['free']
-                            for x in balances if x['asset'] == symbol][0])
-    usdt_balance = float([x['free']
-                         for x in balances if x['asset'] == 'USDT'][0])
-    msg = f'[Before ordered]\n'
-    msg += f'{symbol} balance: {symbol_balance}\n'
-    msg += f'USDT balance: {usdt_balance}\n'
-    tg.send_by_bot(msg)
-
-    symbolUSDT = symbol + 'USDT'
-    if type == "buy":
-        params = {
-            'symbol': symbolUSDT,
-            'side': 'BUY',
-            'type': 'MARKET',
-            'quoteOrderQty': quoteOrderQty,
-        }
-    if type == "sell":
-        price = client.get_avg_price(symbol=symbolUSDT)
-        params = {
-            'symbol': symbolUSDT,
-            'side': 'SELL',
-            'type': 'MARKET',
-            'quoteOrderQty': "{:.2f}".format(symbol_balance * float(price['price']) * cfg.token_remain_rate),
-        }
-
-    response = ""
     try:
-        response = spot.new_order(**params)
-    except Exception as e:
-        response = e.__str__()
-    tg.send_by_bot(json.dumps(response, indent=2))
+        spot = Spot()
+        spot = Spot(key=cfg.biance_api_key, secret=cfg.biance_secrect_key)
+        client = Client(cfg.biance_api_key, cfg.biance_secrect_key)
 
-    balances = spot.account()['balances']
-    symbol_balance = float([x['free']
-                            for x in balances if x['asset'] == symbol][0])
-    usdt_balance = float([x['free']
-                         for x in balances if x['asset'] == 'USDT'][0])
-    msg = f'[After ordered]\n'
-    msg += f'{symbol} balance: {symbol_balance}\n'
-    msg += f'USDT balance: {usdt_balance}\n'
-    tg.send_by_bot(msg)
+        symbol_balance, usdt_balance = getBalance(
+            spot, symbol, '[Before ordered]')
+
+        symbolUSDT = symbol + 'USDT'
+        if type == "buy":
+            params = {
+                'symbol': symbolUSDT,
+                'side': 'BUY',
+                'type': 'MARKET',
+                'quoteOrderQty': quoteOrderQty,
+            }
+        if type == "sell":
+            price = client.get_avg_price(symbol=symbolUSDT)
+            params = {
+                'symbol': symbolUSDT,
+                'side': 'SELL',
+                'type': 'MARKET',
+                'quoteOrderQty': "{:.2f}".format(symbol_balance * float(price['price']) * cfg.token_remain_rate),
+            }
+
+        response = ""
+        try:
+            response = spot.new_order(**params)
+            tg.send_by_bot(json.dumps(response, indent=2))
+        except Exception as e:
+            response = e.__str__()
+            tg.send_by_bot(response)
+
+        symbol_balance, usdt_balance = getBalance(
+            spot, symbol, '[After ordered]')
+
+    except Exception as e:
+        tg.send_by_bot(f'[In Binance Client Error]\n{e.__str__()}')
