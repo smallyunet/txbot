5849d586da96dac29c87d433542869109b057b31
smallyunet <dev.wangyu@gmail.com>
2022-12-08 18:03:11 +0800

    update

--- bi123.py
+++ bi123.py
@@ -88,6 +88,7 @@ def get_mail(retry=0):
                         bc.make_order('sell', k)
 
         # get balance after order
+        total = 0
         msg = f'[All balance]\n'
         spot, client = bc.get_client()
         balance, ubalance = bc.get_balance(client, spot, 'USDT')
@@ -95,6 +96,8 @@ def get_mail(retry=0):
         for k, v in cfg.tokens.items():
             balance, ubalance = bc.get_balance(client, spot, k)
             msg += f'{k}: {ubalance}\n'
+            total += ubalance
+        msg += f'Total: {total}\n'
         tb.send_by_bot(msg)
 
     except Exception as e:
